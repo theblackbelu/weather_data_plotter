61195004f2f594567f306f6eb69861245d5bd215
anjingliaran <kidefuzambranam3115185nhgy@hotmail.com>
2025-01-03 06:47:39 +0000

    fixing weatherData.js

--- weatherData.js
+++ weatherData.js
@@ -4,12 +4,12 @@ const weatherData = {
         '2024-01-01', '2024-01-02', '2024-01-03', '2024-01-04', '2024-01-05',
         '2024-01-06', '2024-01-07', '2024-01-08', '2024-01-09', '2024-01-10'
     ],
-    temperatures: [12, 14, 16, 18, 20, 22, 19, 17, 15, 13],
-    humidity: [65, 68, 70, 72, 75, 78, 73, 71, 69, 66],
-    precipitation: [0, 2, 0, 0, 5, 8, 3, 1, 0, 0]
+    temperatures: [32, 35, 38, 30, 28, 33, 40, 42, 36, 34],
+    humidity: [65, 70, 60, 75, 80, 68, 55, 50, 62, 70],
+    precipitation: [0.1, 0.0, 0.0, 0.5, 1.2, 0.0, 0.0, 0.0, 0.3, 0.1]
 };
 
-// Function to calculate moving average for trend lines
+// Function to calculate moving average for trend analysis
 function calculateMovingAverage(data, windowSize = 3) {
     const movingAverages = [];
     for (let i = 0; i < data.length; i++) {
@@ -24,4 +24,5 @@ function calculateMovingAverage(data, windowSize = 3) {
 }
 
 // Export data and functions
-export { weatherData, calculateMovingAverage };
+window.weatherData = weatherData;
+window.calculateMovingAverage = calculateMovingAverage;
